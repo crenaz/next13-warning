039760b2cb24da69dcd37ce69db63fd177eb6c1e
crenaz <crenaz@gmail.com>
2022-12-03 14:00:25 -0400

    now using typescript, for real

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { use } from "react";
+
+import Image from 'next/image';
+import styles from './page.module.css';
+
+
+
+const fetchMap = new Map<string,Promise<any>>();
+
+function queryClient(name: string, query: () => Promise<any> ) {
+  if (!fetchMap.has(name)) {
+    fetchMap.set(name, query());
+  }
+  return fetchMap.get(name)!;
+}
+
+
 
 export default function Home() {
+  const data =  use(
+    queryClient("hello", () =>
+      fetch("http://localhost:3000/api/hello").then(res => res.json())
+    )
+  );
+  
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -10,33 +33,12 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
+          * Now using {' '}
+          <code className={styles.code}>.tsx</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+{JSON.stringify(data)}
         </div>
       </main>
 
